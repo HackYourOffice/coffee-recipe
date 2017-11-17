83c862dc578b7ec907897430edd43cf487b1f7e9
Robin de Silva Jayasinghe <jayasinghe@synyx.de>
2017-11-17 18:33:01 +0100

    also offer GET for make request

--- src/main/java/org/synyx/coffeecontrol/make/web/MakeController.java
+++ src/main/java/org/synyx/coffeecontrol/make/web/MakeController.java
@@ -2,8 +2,10 @@ package org.synyx.coffeecontrol.make.web;
 
 import liquibase.util.StringUtils;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import org.synyx.coffeecontrol.Loggable;
@@ -29,6 +31,12 @@ public class MakeController implements Loggable {
     @PostMapping("/make")
     public void makeForToken(@RequestBody TokenDto tokenDto) {
 
+        sendCommandIfTokenIsPresent(tokenDto);
+    }
+
+
+    private void sendCommandIfTokenIsPresent(@RequestBody TokenDto tokenDto) {
+
         if (StringUtils.isEmpty(tokenDto.token)) {
             logger().info("received an empty token command. will not look for recipes");
         }
@@ -40,5 +48,16 @@ public class MakeController implements Loggable {
         } else {
             logger().info("did not find recipe for token {}", tokenDto.token);
         }
+    }
+
+
+    @GetMapping("/make")
+    public void makeForTokenGet(@RequestParam(name = "token") final String token,
+        @RequestParam(name = "machine") final String machineId) {
+
+        final TokenDto tokenDto = new TokenDto();
+        tokenDto.token = token;
+        tokenDto.machineId = machineId;
+        sendCommandIfTokenIsPresent(tokenDto);
     }
 }
